32e202360585b6267c0ff1292ea0c680735f2674
tomike19 <kolajoelizabeth@gmail.com>
2022-02-28 23:57:16 -0800

    nav-landing-page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
+import LandingPage from './Landing Page/landing-page';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-       
-      </header>
+    <div className="">
+    <LandingPage/>
     </div>
   );
 }
---
+++ src/Landing Page/landing-page.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import '../css/landing-page.css'
+ 
+const LandingPage  = () => {
+    return (
+        <div>
+             <nav className="navbar navbar-expand-lg bg-light default-layout__inner">
+      <div className="container py-3">
+        <h1 className="navbar-logo">Rockify-music</h1>
+        <i
+          className="navbar-toggler bi bi-list"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"></i>
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="navbarNav">
+          <ul className="navbar-nav d-flex justify-content-end mt-2 mb-lg-0">
+
+
+              <li className="list-item p-2">
+                Home
+              </li>
+
+              <li className="list-item p-2">
+               About
+              </li>
+          <li className="list-item p-2">
+               Pages
+              </li>
+                   <li className="list-item p-2">
+              Contact
+              </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+        </div>
+    );
+}
+ 
+ 
+export default LandingPage  ;
