5013f26275d08b166c31fd6a83d6bb5fa962c0d8
tomike19 <kolajoelizabeth@gmail.com>
2022-03-03 01:00:29 -0800

    fixed artiste details

--- src/Landing Page/landing-page.js
+++ src/Landing Page/landing-page.js
@@ -1,71 +1,80 @@
-import React from "react";
-import "../css/landing-page.css";
-import Image1 from "../images/sia.jpg";
-import Image2 from "../images/celine dion.jpg"
-import Image3 from "../images/aquerio.jpg"
+import React from 'react'
+import '../css/landing-page.css'
+import Image1 from '../images/sia.jpg'
+import Image2 from '../images/celine dion.jpg'
+import Image3 from '../images/aquerio.jpg'
 
 const LandingPage = () => {
   return (
     <div className="home-section">
       <nav className="navbar navbar-expand-lg  default-layout__inner">
         <div className="container py-3">
-          <h1 className="navbar-logo">RockifyMusic</h1>
+          <h1 className="navbar-logo"> RockifyMusic </h1>
         </div>
       </nav>
       <div className=" hero-section">
         <div className="input-searchbar-section">
           <input className="input-search" placeholder="Search for artiste" />
-          <i className="bi bi-search search-icon"></i>
+          <i className="bi bi-search search-icon"> </i>
         </div>
         <div className="card-flex">
-        <div className="card artiste-details-section">
-          <img src={Image1} class="card-img-top artiste-image" alt="sia" />
-          <div className="card-img-overlay d-flex flex-column">
-            <h5 className="card-title">Sia</h5>
-            <div className="image-overlay"></div>
+          <div className="card artiste-details-section">
+            <img src={Image1} class="card-img-top artiste-image" alt="sia" />
+
+            <div className="card-body artiste-details">
+              <p className="card-text artiste-name">Sia </p>
+              <p className="card-text artiste-description">
+                Sia Kate Isobelle Furler is an Australian singer and songwriter.
+                She started her career as a singer in the acid jazz band Crisp
+                in the mid-1990s in Adelaide.
+              </p>
+              <div className="view-more-button-section">
+                <button className="view-more-button"> View more</button>
+              </div>
+            </div>
           </div>
-          
-          <div className="card-body artiste-details">
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
+          <div className="card artiste-details-section">
+            <div>
+              <img
+                src={Image2}
+                class="card-img-top artiste-image"
+                alt="celion-dion"
+              />
+            </div>
+
+            <div className="card-body artiste-details">
+              <p className="card-text">Celine Dion </p>
+              <p className="card-text artiste-description">
+                Céline Marie Claudette Dion CC OQ is a Canadian singer. She is
+                noted for her powerful and technically skilled vocals.Dion is
+                the best-selling Canadian recording artist.
+              </p>
+              <div className="view-more-button-section">
+                <button className="view-more-button"> View more</button>
+              </div>
+            </div>
           </div>
-        </div>
-         <div className="card artiste-details-section">
-          <img src={Image2} class="card-img-top artiste-image" alt="celion-dion" />
-          <div className="card-img-overlay d-flex flex-column">
-            <h5 className="card-title">Sia</h5>
-            <div className="image-overlay"></div>
+          <div className="card artiste-details-section">
+            <div className="">
+              <img src={Image3} class="card-img-top artiste-image" alt="sia" />
+            </div>
+
+            <div className="card-body artiste-details">
+              <p className="card-text">Christina Aguilera</p>
+              <p className="card-text artiste-description">
+                Christina María Aguilera is an American singer, songwriter, and
+                television personality. Known for her four-octave vocal range
+                and ability to sustain high notes.
+              </p>
+              <div className="view-more-button-section">
+                <button className="view-more-button"> View more</button>
+              </div>
+            </div>
           </div>
-          
-          <div className="card-body artiste-details">
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-          </div>
-        </div>
-         <div className="card artiste-details-section">
-         <div className="image-section">
-          <img src={Image3} class="card-img-top artiste-image" alt="sia" />
-         </div>
-          <div className="card-img-overlay d-flex flex-column">
-            <h5 className="card-title">Sia</h5>
-            <div className="image-overlay"></div>
-          </div>
-          
-          <div className="card-body artiste-details">
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-          </div>
-        </div>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default LandingPage;
+export default LandingPage
