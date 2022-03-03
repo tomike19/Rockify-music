7728930cdb49fe612413a81235ca8c3c94fd0bce
tomike19 <kolajoelizabeth@gmail.com>
2022-03-02 21:01:42 -0800

    artiste-details

--- src/Landing Page/landing-page.js
+++ src/Landing Page/landing-page.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import '../css/landing-page.css'
 import Sia from "../images/sia.jpg"
+
 const LandingPage  = () => {
     return (
-        <div>
+        <div className='home-section'>
              <nav className="navbar navbar-expand-lg  default-layout__inner">
       <div className="container py-3">
         <h1 className="navbar-logo">RockifyMusic</h1>
@@ -11,13 +12,13 @@ const LandingPage  = () => {
       
       </div>
     </nav>
-    <div className=' home-section'>
+    <div className=' hero-section'>
     <div className="input-searchbar-section">
      <input className='input-search' placeholder='Search for artiste'/>
      <i className="bi bi-search search-icon"></i>
     </div>
-    <div className="card">
-  <img src={Sia} class="card-img-top" alt="..."/>
+    <div className="card artiste-details-section">
+  <img src={Sia} class="card-img-top artiste-image" alt="sia"/>
   <div class="card-body">
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
   </div>
